d1098993f0f13e6a300712f6b3a3388f1eb060e6
Chih-Yung Liang <cyliang@users.noreply.github.com>
2014-11-06 02:10:07 +0800

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,39 @@
+#include<REG_MPC82G516.H>
+
+void	timer(void);
+void	display();
+
+void main(void)
+{
+	TMOD=6;
+	while(1){
+		display();
+		timer();
+	}
+
+
+}
+
+void timer(void)
+{
+	TH0=251;
+	TL0=251;
+	TR0=1;
+	while(1){
+		if(TF0==1){
+			TF0=0;
+			break;
+		}
+	}
+
+}
+
+void display()
+{
+	static char num = 0;
+	P1 = 0;
+	P2 = num;
+	
+	if(++num > 9)
+		num = 0;
+}
