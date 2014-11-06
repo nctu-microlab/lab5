a0b48c5349793556ec0aadc83603f2bd70f93a0b
Chih-Yung Liang <cyliang@users.noreply.github.com>
2014-11-06 20:08:28 +0800

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,11 +1,16 @@
 #include<REG_MPC82G516.H>
 
+sbit start_btn = P3 ^ 1;
+sbit add_btn = P3 ^ 2;
+
+void show_digit(char pos, char num);
+
 void main(void)
 {
-	sbit start_btn = P3 ^ 0;
-	sbit add_btn = P3 ^ 1;
 	
 	char num1 = 0, num2 = 0;
+	char counter1 = 0, counter2 = 0;
+	bit add_click = 0;
 	
 	P3 = 0xFF;
 	TMOD = 1;
@@ -13,9 +18,10 @@ void main(void)
 	TL0 = 176;
 	TR0 = 0;
 	
-	char counter1 = 0, counter2 = 0;
 	while(1){
-		if(!add_btn) {
+		if(!add_btn && !add_click) {
+			add_click = 1;
+			
 			if(TR0) {
 				TR0 = 0;
 			} else {
@@ -23,11 +29,17 @@ void main(void)
 				++num2;
 			}
 		}
+		if(add_btn) {
+			add_click = 0;
+		}
+		
 		if(!start_btn) {
 			TR0 = 1;
 		}
 		
 		if(TF0) {
+			TF0 = 0;
+			
 			if(++counter1 == 40) {
 				++num1;
 				counter1 = 0;
@@ -48,14 +60,14 @@ void main(void)
 }
 
 void show_digit(char pos, char num) {
-	char i;
+	unsigned char i;
 	
-	P1 = pos;
+	P1 = ~pos;
 	P2 = num / 10;
 	
 	for(i=0; i<250; i++);
 	
-	P1 = pos >> 1;
+	P1 = ~(pos >> 1);
 	P2 = num % 10;
 	
 	for(i=0; i<250; i++);
